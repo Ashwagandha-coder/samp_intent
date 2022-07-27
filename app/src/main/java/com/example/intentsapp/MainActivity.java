d13232f8c34df7e4560073dd1bd7f61fc3a5aa46
Ashwaganha-cader <max.cacz@yandex.ru>
2022-07-27 15:30:34 +0500

    add setOnClickListener in button

--- app/src/main/java/com/example/intentsapp/MainActivity.java
+++ app/src/main/java/com/example/intentsapp/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.intentsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText textEntry;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        textEntry = findViewById(R.id.et_field);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(MainActivity.this, "Button was clicked", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
     }
 }
